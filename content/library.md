80229b7f57b68aa957c7ec9bda05c1a3f0626eb3
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2023-03-08 02:12:01 +0530

    added graph in library

--- content/library.md
+++ content/library.md
@@ -10,6 +10,9 @@ ShowReadingTime: false
 ---
 
 
+![Books read per year](/images/books_read_per_year.png)
+
+
 This is a list of all the books I can remember reading. Since I remember them, most are pretty good. Again, they are in roughly descending order of subjective quality. If you want to see books I hope to read in the future, see my anti-library.
 
 Starting in 2021, I decided to track all the books I read. 
---
+++ scripts/count_books.py
@@ -0,0 +1,39 @@
+import csv
+import os
+import yaml
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+def main():
+    # Load the books YAML file
+    with open("data/books.yaml", "r") as f:
+        books = yaml.safe_load(f)
+
+    # Initialize a dictionary to store the book count for each year
+    book_counts = {}
+
+    # Loop through each book and count the books read in each year
+    for book in books:
+        rating = int(book["rating"])
+        date_str = book["date"].strftime("%Y-%m-%d")
+        year = book["date"].year
+        month = book["date"].month
+        if year in book_counts:
+            book_counts[year] += 1
+        else:
+            book_counts[year] = 1
+
+    # Sort the book counts by year
+    book_counts = dict(sorted(book_counts.items()))
+
+    # Plot the book counts
+    plt.bar(list(book_counts.keys()), list(book_counts.values()), align='center')
+    plt.xticks(list(book_counts.keys()))
+    plt.xlabel("Year")
+    plt.ylabel("Number of Books Read")
+    plt.title("Books Read Per Year")
+    plt.savefig("static/images/books_read_per_year.png")
+
+if __name__ == "__main__":
+    main()
