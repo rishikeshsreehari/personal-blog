c78aca8ac90d6b083c62da8faecba2be41959afa
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2024-06-21 00:31:30 +0400

    Fixed issues with antilibrary and contact

--- public/js/show-more-books.js
+++ public/js/show-more-books.js
@@ -1,30 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const initialDisplayCount = {{ .Site.Params.BookShelfDisplayCount }};
+document.addEventListener('DOMContentLoaded', function() {
+  const initialDisplayCount = INITIAL_DISPLAY_COUNT;
   let displayedCount = initialDisplayCount;
   const books = document.querySelectorAll('.book');
   const showMoreButton = document.getElementById('show-more');
   
-    // Hide all books initially
-    books.forEach((book, index) => {
-      if (index >= initialDisplayCount) {
-        book.style.display = 'none';
+  // Hide all books initially
+  books.forEach(function(book, index) {
+    if (index >= initialDisplayCount) {
+      book.style.display = 'none';
+    }
+  });
+
+  // Define the function to show more books
+  window.showMoreBooks = function() {
+    const nextCount = displayedCount + initialDisplayCount;
+    books.forEach(function(book, index) {
+      if (index < nextCount) {
+        book.style.display = 'block';
       }
     });
-  
-    // Show more books on button click
-    window.showMoreBooks = function () {
-      const nextCount = displayedCount + initialDisplayCount;
-      books.forEach((book, index) => {
-        if (index < nextCount) {
-          book.style.display = 'block';
-        }
-      });
-      displayedCount = nextCount;
-  
-      // Hide the button if all books are displayed
-      if (displayedCount >= books.length) {
-        showMoreButton.style.display = 'none';
-      }
-    };
-  });
-  
+    displayedCount = nextCount;
+
+    // Hide the button if all books are displayed
+    if (displayedCount >= books.length) {
+      showMoreButton.style.display = 'none';
+    }
+  };
+});
