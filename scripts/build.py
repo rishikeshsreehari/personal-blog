b39f9b7b79bfeb796b24ee2c2ca8b0372846d678
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2024-11-20 23:38:43 +0400

    test 2

--- scripts/build.py
+++ scripts/build.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     scripts = [
         'scripts/count_books.py',
         'scripts/create_worldmap.py',
-        #'scripts/update_version.py', #Disabled to test
+        #'scripts/update_version.py', #Disabled to testt
         'scripts/then.py'
     ]
     
